8d422fe3a51cbfb5f8495b63c2a3d668e4276be4
SimonDesc <pro.descombes@gmail.com>
2024-04-01 11:18:18 +0200

    fix: eslint

--- utils/redis.js
+++ utils/redis.js
@@ -2,35 +2,35 @@ import { createClient } from 'redis';
 import { promisify } from 'util';
 
 class RedisClient {
-	constructor() {
-		this.client = createClient();
-		this.getAsync = promisify(this.client.get).bind(this.client);
+  constructor() {
+    this.client = createClient();
+    this.getAsync = promisify(this.client.get).bind(this.client);
 
-		this.client.on('connect', () => {
-			// console.log('Redis client connected to the server');
-		});
+    this.client.on('connect', () => {
+      // console.log('Redis client connected to the server');
+    });
 
-		this.client.on('error', (error) => {
-			console.log(`Redis client not connected to the server: ${error.message}`);
-		});
-	}
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to the server: ${error.message}`);
+    });
+  }
 
-	isAlive() {
-		return this.client.connected;
-	}
+  isAlive() {
+    return this.client.connected;
+  }
 
-	async get(key) {
-		const value = await this.getAsync(key);
-		return value;
-	}
+  async get(key) {
+    const value = await this.getAsync(key);
+    return value;
+  }
 
-	async set(key, value, ttl) {
-		this.client.setex(key, ttl, value);
-	}
+  async set(key, value, duration) {
+    this.client.setex(key, duration, value);
+  }
 
-	async del(key) {
-		this.client.del(key);
-	}
+  async del(key) {
+    this.client.del(key);
+  }
 }
 
 const redisClient = new RedisClient();
