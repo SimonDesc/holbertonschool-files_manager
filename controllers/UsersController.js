043f1b995a772bbf676380dd0b8cee06cdeff7e9
SimonDesc <pro.descombes@gmail.com>
2024-04-02 11:15:23 +0200

    fix: cleaner code for checker

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -4,25 +4,29 @@ const crypto = require('crypto');
 
 class UsersController {
   static async postNew(req, res) {
-    // console.log(req.body)
     const { email, password } = req.body;
-    if (!email) return res.status(400).send({ error: 'Missing email' });
-    if (!password) return res.status(400).send({ error: 'Missing password' });
 
-    // if the email already exists in DB, return an error Already exist with a status code 400
+    if (!email) {
+      return res.status(400).send({ error: 'Missing email' });
+    }
+
+    if (!password) {
+      return res.status(400).send({ error: 'Missing password' });
+    }
+
     const { usersCollection } = dbClient;
     const user = await usersCollection.findOne({ email });
-    if (user) return res.status(400).send({ error: 'Already exist' });
 
-    // The password must be stored after being hashed in SHA1
-    const shasum = crypto.createHash('sha1');
-    shasum.update(password);
-    const hashedPassword = shasum.digest('hex');
+    if (user) {
+      return res.status(400).send({ error: 'Already exist' });
+    }
+
+    const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
     const newUser = await usersCollection.insertOne({
       email,
       password: hashedPassword,
     });
-    // console.log(newUser.insertedId, email);
+
     return res.status(201).send({ id: newUser.insertedId, email });
   }
 }
