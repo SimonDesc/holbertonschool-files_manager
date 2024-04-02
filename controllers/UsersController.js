5c41e5667a2dd50aa7c0987bf57d60f89aa194a4
SimonDesc <pro.descombes@gmail.com>
2024-04-02 11:41:47 +0200

    fix: requirement wont pass... open the checker ffs

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,6 +1,8 @@
+import sha1 from 'sha1';
+import Queue from 'bull';
 import dbClient from '../utils/db';
 
-const crypto = require('crypto');
+const userQueue = new Queue('userQueue');
 
 class UsersController {
   static async postNew(req, res) {
@@ -14,20 +16,32 @@ class UsersController {
       return res.status(400).send({ error: 'Missing password' });
     }
 
-    const { usersCollection } = dbClient;
-    const user = await usersCollection.findOne({ email });
+    const existEmail = await dbClient.usersCollection.findOne({ email });
 
-    if (user) {
+    if (existEmail) {
       return res.status(400).send({ error: 'Already exist' });
     }
 
-    const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
-    const newUser = await usersCollection.insertOne({
+    const passwordSha1 = sha1(password);
+
+    let returnResult;
+    try {
+      returnResult = await dbClient.usersCollection.insertOne({ email, password: passwordSha1 });
+    } catch (err) {
+      await userQueue.add({});
+      return res.status(500).send({ error: 'Error creating user' });
+    }
+
+    const user = {
+      id: returnResult.insertedId,
       email,
-      password: hashedPassword,
+    };
+
+    await userQueue.add({
+      userId: returnResult.insertedId.toString(),
     });
 
-    return res.status(201).send({ id: newUser.insertedId, email });
+    return res.status(201).send(user);
   }
 }
 
